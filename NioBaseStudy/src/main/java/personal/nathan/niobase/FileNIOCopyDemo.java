84b4e4fc442ee66bf2f4a4a28cc3759aabce50e3
za-zhangwei002 <wei.zhang@zatech.com>
2019-10-29 18:30:44 +0800

    update

--- NioBaseStudy/src/main/java/personal/nathan/niobase/FileNIOCopyDemo.java
+++ NioBaseStudy/src/main/java/personal/nathan/niobase/FileNIOCopyDemo.java
@@ -27,7 +27,6 @@ public class FileNIOCopyDemo {
 
     }
 
-
     /**
      * 复制两个资源目录下的文件
      */
@@ -43,7 +42,6 @@ public class FileNIOCopyDemo {
         nioCopyFile(srcPath, destdePath);
     }
 
-
     /**
      * 复制文件
      *
@@ -51,19 +49,14 @@ public class FileNIOCopyDemo {
      * @param destPath
      */
     public static void nioCopyFile(String srcPath, String destPath) {
-
         File srcFile = new File(srcPath);
         File destFile = new File(destPath);
-
         try {
             //如果目标文件不存在，则新建
             if (!destFile.exists()) {
                 destFile.createNewFile();
             }
-
-
             long startTime = System.currentTimeMillis();
-
             FileInputStream fis = null;
             FileOutputStream fos = null;
             FileChannel inChannel = null;
@@ -73,15 +66,12 @@ public class FileNIOCopyDemo {
                 fos = new FileOutputStream(destFile);
                 inChannel = fis.getChannel();
                 outchannel = fos.getChannel();
-
                 int length = -1;
                 ByteBuffer buf = ByteBuffer.allocate(1024);
                 //从输入通道读取到buf
                 while ((length = inChannel.read(buf)) != -1) {
-
                     //翻转buf,变成成读模式
                     buf.flip();
-
                     int outlength = 0;
                     //将buf写入到输出的通道
                     while ((outlength = outchannel.write(buf)) != 0) {
@@ -90,8 +80,6 @@ public class FileNIOCopyDemo {
                     //清除buf,变成写入模式
                     buf.clear();
                 }
-
-
                 //强制刷新磁盘
                 outchannel.force(true);
             } finally {
@@ -107,5 +95,4 @@ public class FileNIOCopyDemo {
             e.printStackTrace();
         }
     }
-
 }
